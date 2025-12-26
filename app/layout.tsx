3861c526bb0a758fe5c09d21ef7a2533a711b8c4
Nadya <nadya.123140167@student.itera.ac.id>
2025-12-26 16:49:16 +0700

    debug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./global.css";
+import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://innocentresources.com"),
